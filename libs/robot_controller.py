47a5a5ec0874406905bb0fbd08cd36b978c2d590
grayledw <grayledw@rose-hulman.edu>
2017-10-29 20:58:52 -0400

    Working on M4

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -33,9 +33,9 @@ class Snatch3r(object):
     def turn_degrees(self, degrees_to_turn, turn_speed):
         time_s = 1  # Any value other than 0.
         while time_s != 0:
-            angle_time = degrees_to_turn / turn_speed
-            self.left_motor.run_to_rel_pos(speed_sp=(turn_speed * -1), time_sp=angle_time, stop_action=ev3.Motor.STOP_ACTION_BRAKE)
-            self.right_motor.run_to_rel_pos(speed_sp=turn_speed, time_sp=angle_time, stop_action=ev3.Motor.STOP_ACTION_BRAKE)
+            # angle_time = degrees_to_turn / turn_speed
+            self.left_motor.run_to_rel_pos(position_sp=degrees_to_turn, speed_sp=(turn_speed * -1), stop_action=ev3.Motor.STOP_ACTION_BRAKE)
+            self.right_motor.run_to_rel_pos(position_sp=degrees_to_turn, speed_sp=(turn_speed), stop_action=ev3.Motor.STOP_ACTION_BRAKE)
             # self.left_motor.speed_sp = turn_speed
             # self.right_motor.speed_sp = -turn_speed
             self.right_motor.wait_while(ev3.Motor.STATE_RUNNING)
@@ -44,7 +44,7 @@ class Snatch3r(object):
         ev3.Sound.beep().wait()
 
 
-           
+
 
 
     # TODO: Implement the Snatch3r class as needed when working the sandox exercises
